763c05c329f407184d82749826daeadc75b4674e
ec2-user <ec2-user@ip-172-31-1-45.eu-west-2.compute.internal>
2022-05-04 18:25:04 +0100

    commit

--- parentheses_cli.py
+++ parentheses_cli.py
@@ -5,14 +5,3 @@ while True:
     api_url = "http://localhost:10002/parentheses/"+str
     response = requests.get(api_url)
     print(response.json())
-
-
-# Driver code
-#string = "{[]{()}}"
-#print(string,"-", check(string))
-#
-#string = "[{}{})(]"
-#print(string,"-", check(string))
-#
-#string = "((()"
-#print(string,"-",check(string))
