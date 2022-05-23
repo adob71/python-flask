b6cd804f27ab8a281916babc1d9e90434a8789b2
ec2-user <ec2-user@ip-172-31-1-45.eu-west-2.compute.internal>
2022-05-23 14:20:05 +0100

    commit

--- greeting-api-client.py
+++ greeting-api-client.py
@@ -2,7 +2,6 @@ import requests
 
 while True:
     str = input("Enter a name:") or "()"
-#    api_url = "http://localhost:10002/parentheses/"+str
-    api_url = "http://runbook.expert:10003/greeting?name="+str
+    api_url = "https://runbook.expert:10003/greeting?name="+str
     response = requests.get(api_url)
     print(response.json())
--- square-api-client.py
+++ square-api-client.py
@@ -2,7 +2,6 @@ import requests
 
 while True:
     x = input("Enter a number:") or "1"
-#    api_url = "http://localhost:10001/square/"+x
     api_url = "https://runbook.expert:10001/square/"+x
     response = requests.get(api_url)
     print(response.json())
