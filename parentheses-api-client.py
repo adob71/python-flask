038910caef42308a603f33bf7b0e508c4e3c2abf
ec2-user <ec2-user@ip-172-31-1-45.eu-west-2.compute.internal>
2022-07-13 09:05:16 +0100

    commit

--- parentheses-api-client.py
+++ parentheses-api-client.py
@@ -2,6 +2,6 @@ import requests
 
 while True:
     string = input("Enter a string:") or "()"
-    api_url = "https://runbook.expert:10002/parentheses/"+string
+    api_url = "https://runbook.adob71.com:10002/parentheses/"+string
     response = requests.get(api_url)
     print(response.json())
--- square-api-client.py
+++ square-api-client.py
@@ -2,6 +2,6 @@ import requests
 
 while True:
     x = input("Enter a number:") or "1"
-    api_url = "https://runbook.expert:10001/square/"+x
+    api_url = "https://runbook.adob71.com:10001/square/"+x
     response = requests.get(api_url)
     print(response.json())
